219d6c8af9fd43d5b828dad9225ba2566a507baa
Gandharva Manoj Vaidya <gandharvavaidya11@gmail.com>
2025-07-02 15:53:12 +0530

    Basics

---
+++ Conditional_Expression.py
@@ -0,0 +1,10 @@
+name = input("Please enter your name: ")
+
+if name == "Gandharva":
+    print("Access Granted.")
+
+elif name == "Vaidya":
+    print("Access Granted.")
+
+else:
+    print("Access Denied.")
---
+++ While_loop.py
@@ -0,0 +1,7 @@
+count = 1
+
+while count <= 5:
+    print(count)
+    count += 1  # Increase the value by 1 in each loop
+
+
